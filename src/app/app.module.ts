d87de09a827119d49f349d40bd8fb843c9c4346f
unknown <sotiriskarageorgopoulos@gmail.com>
2018-07-20 11:53:57 +0300

    Angular Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { FooterComponent } from './footer/footer.component';
-
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
