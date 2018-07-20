1fb9a51aa6861a35cc1d8baef5b75481295e313d
unknown <sotiriskarageorgopoulos@gmail.com>
2018-07-20 11:20:02 +0300

    Angular Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
