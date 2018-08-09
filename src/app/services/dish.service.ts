7129cfe351e55c0e01a1c042a821c2342c1c2bbc
unknown <sotiriskarageorgopoulos@gmail.com>
2018-08-09 15:59:26 +0300

    reactive programming

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable , of} from 'rxjs';
+
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 
@@ -7,11 +9,11 @@ export class DishService {
 
  constructor() { }
 
- getDishes(): Dish[] {
-   return DISHES;
+ getDishes(): Observable<Dish[]> {
+  return Observable.of(DISHES).delay(2000);
  }
-
- getDish(id: number): Dish {
-  return DISHES.filter((dish) => (dish.id === id))[0];
+ 
+ getDish(id: number): Observable<Dish> {
+  return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(2000);
 }
 }
